10db3828bddadae7a8491628b2ee5b6a20c7fdc7
xJoestar <patrickviolin08@gmail.com>
2021-10-06 20:19:25 -0300

    First Commit

---
+++ app/src/main/java/br/com/etecia/loginapp/CreateAccountActivity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.loginapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_criar_conta);
+    }
+}
---
+++ app/src/main/java/br/com/etecia/loginapp/MainActivity.java
@@ -0,0 +1,40 @@
+package br.com.etecia.loginapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private Button signUpApp;
+    private Button loginApp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        signUpApp = findViewById(R.id.sign_up_app);
+        loginApp = findViewById(R.id.login_app);
+
+        signUpApp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), CreateAccountActivity.class));
+            }
+        });
+
+        loginApp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        });
+    }
+
+    public void sairSistema(View view) {
+        finish();
+    }
+}
